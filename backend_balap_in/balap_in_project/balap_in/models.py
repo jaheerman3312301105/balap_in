3f5435074b756c6be9280f1b7896ca11bf06449f
jaheerman3312301105 <frdhn2323@gmail.com>
2024-10-31 23:06:16 +0700

    update database

--- backend_balap_in/balap_in_project/balap_in/models.py
+++ backend_balap_in/balap_in_project/balap_in/models.py
@@ -1,3 +1,107 @@
 from django.db import models
 
 # Create your models here.
+class Pengguna(models.Model): 
+    id_pengguna = models.AutoField(primary_key=True)
+    nama = models.CharField(max_length=20, null=True, blank=True)
+    alamat = models.CharField(max_length=20, null=True, blank=True)
+
+    class Meta :
+        db_table ='pengguna'
+
+class Laporan(models.Model):
+    id_laporan = models.AutoField(primary_key=True)
+    id_pengguna = models.ForeignKey(Pengguna, on_delete=models.CASCADE)
+    id_peta = models.ForeignKey('Peta' , on_delete=models.CASCADE)
+    gambar = models.BinaryField(null=True, blank=True)
+    jenis = models.CharField(
+        max_length=20,
+        choices=[('jalan', 'Jalan'), ('lampu_jalan', 'Lampu Jalan'), ('jembatan', 'Jembatan')],
+        null=True, blank=True
+    )
+    judul = models.CharField(max_length=20, null=True, blank=True)
+    deskripsi = models.CharField(max_length=100, null=True, blank=True)
+    persentase = models.FloatField(null=True, blank=True)
+    cuaca = models.CharField(
+        max_length=20,
+        choices=[('hujan', 'Hujan'), ('cerah', 'Cerah')],
+        null = True, blank=True
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=[('selesai', 'Selesai'), ('draf', 'Draf')],
+    )
+    tgl_lapor = models.DateTimeField(auto_now=True)
+    tingkat_urgent = models.IntegerField(
+        null=True, blank=True
+    )
+
+    class Meta:
+        db_table = 'laporan'
+
+class Peta(models.Model):
+    id_peta = models.AutoField(primary_key=True)
+    id_laporan = models.ForeignKey(Laporan, on_delete=models.CASCADE)
+    alamat = models.CharField(
+        max_length=255,
+        null=True, blank=True
+    )
+    jalan = models.CharField(
+        max_length=255,
+        null=True, blank=True
+    )
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'peta'
+
+class Rekomendasi(models.Model):
+    id_rekomendasi = models.AutoField(primary_key=True)
+    id_laporan = models.ForeignKey(Laporan, on_delete=models.CASCADE)
+    id_peta = models.ForeignKey(Peta, on_delete=models.CASCADE)
+    status_urgent = models.CharField(
+        max_length=10,
+        choices=[('tinggi', 'Tinggi'), ('sedang', 'Sedang'), ('rendah', 'Rendah'), ('belum', 'Belum')],
+        null=True, blank=True
+    )
+    jumlah_laporan = models.IntegerField(
+        null=False, 
+    )
+
+    class Meta:
+        db_table = 'rekomendasi'
+
+class Analisis(models.Model):
+    id_analisis = models.AutoField(primary_key=True)
+    id_rekomendasi = models.ForeignKey(Rekomendasi, on_delete=models.CASCADE)
+    tgl_analisis = models.DateTimeField(
+        auto_now=True
+    )
+    jumlah_urgen = models.IntegerField(
+        null=True, blank=True
+    )
+    jumlah_laporan = models.IntegerField(
+        null=True, blank=True
+    )
+    jenis_infrastruktur = models.CharField(
+        max_length=20,
+        choices=[('jalan', 'Jalan'), ('lampu_jalan', 'Lampu Jalan'), ('jembatan', 'Jembatan')],
+        null=True, blank=True
+    )
+
+    class Meta:
+        db_table = 'analisis'
+
+class Notifikasi(models.Model):
+    id_notifikasi = models.AutoField(primary_key=True)
+    id_rekomendasi = models.ForeignKey(Rekomendasi, on_delete=models.CASCADE)
+    id_analisis = models.ForeignKey(Analisis, on_delete=models.CASCADE)
+    pesan = models.CharField(
+        max_length=255,
+        null=False,
+    )
+    tgl_notif = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'notifikasi'
--- backend_balap_in/balap_in_project/settings.py
+++ backend_balap_in/balap_in_project/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import pymysql
+pymysql.install_as_MySQLdb()
 
 from pathlib import Path
 
@@ -82,8 +84,13 @@ WSGI_APPLICATION = 'balap_in_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'balap_in',  
+        'USER': 'root',     
+        'PASSWORD': '',    
+        'HOST': '127.0.0.1',       
+        'PORT': '3306',
+ 
     }
 }
 
