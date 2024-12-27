409de99f0282fc650eabe2871107029719ca2f79
jaheerman3312301105 <frdhn2323@gmail.com>
2024-12-27 11:39:00 +0700

    update bagian draft laporan sampai finish sampai bagian draft gambar (hapus log draft)

--- frontend_balap_in/lib/controller/controller_draft.dart
+++ frontend_balap_in/lib/controller/controller_draft.dart
@@ -17,36 +17,26 @@ class ControllerDraft {
       if (judulController.text.isNotEmpty) {
         await prefs.remove('judul');
         await prefs.setString('judul', judulController.text);
-        final judul = prefs.getString('judul');
-        print('judul : $judul');
       } 
       
       if (deskripsiController.text.isNotEmpty) {
         await prefs.remove('deskripsi');
         await prefs.setString('deskripsi', deskripsiController.text);
-        final deskripsi = prefs.getString('deskripsi');
-        print('deskripsi : $deskripsi');
       }
 
       if (selectedJenis != 'Jalan') {
         await prefs.remove('jenis');
         await prefs.setString('jenis', selectedJenis);
-        final jenis = prefs.getString('jenis');
-        print('jenis : $jenis');
       }
 
       if (selectedCuaca != 'Hujan') {
         await prefs.remove('cuaca');
         await prefs.setString('cuaca', selectedCuaca);
-        final cuaca = prefs.getString('cuaca');
-        print('cuaca : $cuaca');
       }
 
       if (currentSliderValue != 0.0) {
         await prefs.remove('persentase');
         await prefs.setDouble('persentase', currentSliderValue);
-        final persentase = prefs.getDouble('persentase');
-        print('persentase : $persentase');
       }
 
       if (pickedLocationaLat != null && pickedLocationLong != null && pickedLocationaLat != 0.0 && pickedLocationLong != 0.0) {
@@ -54,18 +44,11 @@ class ControllerDraft {
         await prefs.remove('longitude');
         await prefs.setDouble('latitude', pickedLocationaLat);
         await prefs.setDouble('longitude', pickedLocationLong);
-
-        final latitude = prefs.getDouble('latitude');
-        final longitude = prefs.getDouble('longitude');
-        print('latitude : $latitude');
-        print('longitude : $longitude');
       }
 
       if (gambarfix != null) {
         await prefs.remove('gambar');
         await prefs.setString('gambar', gambarfix);
-        final gambar = prefs.getString('gambar');
-        print('gambar : $gambar');
       }
       
       return 'Laporan berhasil disimpan';
