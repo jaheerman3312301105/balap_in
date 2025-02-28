e21f895d1cbe8d16232d6041e213a61f82b9ccfc
jaheerman3312301105 <frdhn2323@gmail.com>
2025-02-28 13:49:39 +0700

    update host alibaba cloud

--- frontend_balap_in/lib/api/host.dart
+++ frontend_balap_in/lib/api/host.dart
@@ -5,4 +5,5 @@
 
 // const host = 'http://10.0.2.2:8000';
 // const host = 'http://202.10.36.217:8000';
-const host = 'https://flowing-definite-walrus.ngrok-free.app';
+// const host = 'https://flowing-definite-walrus.ngrok-free.app';
+const host = 'http://8.215.4.209:8000';
