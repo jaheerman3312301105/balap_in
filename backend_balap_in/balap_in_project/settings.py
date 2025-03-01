c4509d16b0992cf1362717d32983884c6eb64a90
jaheerman3312301105 <frdhn2323@gmail.com>
2025-03-01 23:28:24 +0700

    update cors alibaba 2c4g

--- backend_balap_in/balap_in_project/settings.py
+++ backend_balap_in/balap_in_project/settings.py
@@ -79,6 +79,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://202.10.36.217",
     "http://8.215.4.209:8000",
+    "http://147.139.241.1:8000"
 ]
 
 ROOT_URLCONF = 'balap_in_project.urls'
--- frontend_balap_in/lib/api/host.dart
+++ frontend_balap_in/lib/api/host.dart
@@ -6,4 +6,5 @@
 // const host = 'http://10.0.2.2:8000';
 // const host = 'http://202.10.36.217:8000';
 // const host = 'https://flowing-definite-walrus.ngrok-free.app';
-const host = 'http://8.215.4.209:8000';
+// const host = 'http://8.215.4.209:8000';
+const host = 'http://147.139.241.1:8000';
